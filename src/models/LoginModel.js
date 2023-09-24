d1c51c2677467884d85183eb2fdece7a2df00621
Diogo <di_smgomes@hotmail.com>
2023-09-24 12:29:42 -0300

    hashing user password and checking if email already exists in DB

--- src/models/LoginModel.js
+++ src/models/LoginModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator')
+const bcryptjs = require('bcryptjs');
 
 const LoginSchema = new mongoose.Schema({
   email: { type: String, required: true },
@@ -18,6 +19,13 @@ class Login {
   async register() {
     this.validate();
     if (this.errors.length > 0) return
+    
+    await this.userExists();
+    
+    if (this.errors.length > 0) return
+    
+    const salt = bcryptjs.genSaltSync()
+    this.body.password = bcryptjs.hashSync(this.body.password, salt);
 
     try {
       this.user = await LoginModel.create(this.body);
@@ -26,10 +34,15 @@ class Login {
     }
   }
 
+  async userExists() {
+    const user = await LoginModel.findOne({ email: this.body.email });
+    if (user) this.errors.push('User already exists');
+  }
+
   validate() {
     this.cleanUp();
-    if (!validator.isEmail(this.body.email)) this.errors.push('email is not a valid email')
-    if (this.body.password.length < 3 || this.body.password.length > 18) this.errors.push('password must be between 2 and 18 characters')
+    if (!validator.isEmail(this.body.email)) this.errors.push('Email is not a valid email')
+    if (this.body.password.length < 3 || this.body.password.length > 18) this.errors.push('Password must be between 2 and 18 characters')
   }
 
   cleanUp() {
