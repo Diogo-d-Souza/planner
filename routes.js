320cf56cb2cd9ac56316b53eb8f222e1bc1bb2a0
Diogo <di_smgomes@hotmail.com>
2023-09-27 16:24:54 -0300

    contacts register developed

--- routes.js
+++ routes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const route = express.Router();
 const homeController = require('./src/controllers/homeController');
 const loginController = require('./src/controllers/loginController');
+const contactController = require('./src/controllers/contactController');
+const { loginRequired } = require('./src/middlewares/middleware')
 
 // Home router
 route.get('/', homeController.index);
@@ -11,5 +13,11 @@ route.get('/login', loginController.index)
 route.post('/login/register', loginController.register)
 route.post('/login/signin', loginController.login)
 route.get('/login/logout', loginController.logout)
+
+// Contact router
+route.get('/contact', loginRequired, contactController.index)
+route.post('/contact/register', loginRequired, contactController.register)
+route.get('/contact/:id', loginRequired, contactController.editIndex)
+
 
 module.exports = route;
---
+++ src/models/ContactModel.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const ContactSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  lastname: { type: String, required: false, default: '' },
+  email: { type: String, required: false, default: '' },
+  phone: { type: String, required: false, default: '' },
+  createdAt: { type: Date, default: Date.now },
+  
+});
+
+const ContactModel = mongoose.model('Contact', ContactSchema);
+
+class Contact {
+  constructor(body) {
+    this.body = body;
+    this.errors = [];
+    this.contact = null;
+  }
+
+  static async searchForId(id) {
+    if (typeof id !== 'string') return;
+    const user = await ContactModel.findById(id);
+    return user;
+  }
+
+  async register() {
+    this.validate();
+    if (this.errors.length > 0) return;
+    this.contact = await ContactModel.create(this.body);
+  }
+
+  validate() {
+    this.cleanUp();
+    if (this.body.email && !validator.isEmail(this.body.email)) this.errors.push('Email is not a valid email')
+    if (!this.body.name) this.errors.push('Name is a required field')
+    if (!this.body.email && !this.body.phone) this.errors.push('You must provide a phone number or email address')
+  }
+
+  cleanUp() {
+    for (const key in this.body) {
+      if (typeof this.body[key] !== 'string') {
+        this.body[key] = '';
+      }
+    }
+    this.body = {
+      name: this.body.name,
+      lastname: this.body.lastname,
+      email: this.body.email,
+      phone: this.body.phone,
+    };
+  }
+
+}
+
+module.exports = Contact;
